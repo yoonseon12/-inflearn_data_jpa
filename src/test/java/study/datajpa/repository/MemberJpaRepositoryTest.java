fb32df70f5ad7136f23ccdfc04c2d09127d2358b
lys <yoonseon12@naver.com>
2023-05-17 16:04:22 +0900

    [프로젝트 생성 및 JPA 간단 예제 작성]
    간단예제
    1. 회원가입
    - 순수 JPA로 회원가입 구현 및 테스트코드 작성
    - Spring Data JPA로 회원가입 구현 및 테스트코드 작성

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,28 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+@SpringBootTest
+@Transactional(readOnly = true)
+@Rollback(false)
+class MemberJpaRepositoryTest {
+    @Autowired
+    public MemberJpaRepository memberJpaRepository;
+
+    @Test
+    @Transactional
+    void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        Assertions.assertThat(savedMember).isEqualTo(findMember);
+    }
+}
---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,29 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+    @Autowired
+    public MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        Assertions.assertThat(savedMember).isEqualTo(findMember);
+    }
+
+}
